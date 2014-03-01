b6fcc57c4cb7cc87080c1f59482317c0b8aac687
Spencer Sellers <spencesellers@gmail.com>
2014-03-01 15:28:50 -0600

    Added rocks. Working on tilesheet of rocks.

--- static/game.js
+++ static/game.js
@@ -9,16 +9,20 @@ function preload() {
     game.load.image('ship', 'assets/ship.png');
     game.load.image('bg', 'assets/spacebg.png');
     game.load.image('bullet', 'assets/bullet.png');
+    game.load.image('rock1', 'assets/rock1.png');
+    game.load.tileset('rocks', 'assets/rocksheet.png', 64, 64);
 }
  
 function create() {
-    game.world.setBounds(0,0,10000, 10000);
+    game.world.setBounds(0,0,5000, 5000);
     var background = game.add.tileSprite(0,0,game.world.width,game.world.height, 'bg');
 
      // Bullets
     bullets = game.add.group();
     bullets.createMultiple(100, 'bullet');
     bullets.setAll('outOfBoundsKill', true);
+    //bullets.setAll('body.immovable', true);
+
     //bullets.setAll('lifespan', 1000);
     bullets.callAll('anchor.setTo', null, 0.5, 0.5);
     
@@ -28,9 +32,17 @@ function create() {
     player.body.collideWorldBounds = true;
     game.camera.follow(player);
 
+    // Rocks
+    rocks = game.add.group();
+    makeRocks();
+    
+
 }
  
 function update() {
+    game.physics.collide(player, rocks);
+    game.physics.collide(bullets, rocks, onBulletHitsRock);
+    
     if (game.input.keyboard.isDown(Phaser.Keyboard.W)){
         thrust(10);
     }
@@ -38,10 +50,10 @@ function update() {
         thrust(-5);
     }
     if (game.input.keyboard.isDown(Phaser.Keyboard.A)){
-        player.rotation += 0.05;
+        player.rotation += 0.2;
     }
     if (game.input.keyboard.isDown(Phaser.Keyboard.D)){
-        player.rotation -= 0.05;
+        player.rotation -= 0.2;
     }
     if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
         fire();
@@ -85,4 +97,22 @@ function fire(){
 
     bullet.lifespan = 5000;
     timeLastFired = game.time.now;
+}
+
+function makeRocks(){
+    for (var i=0; i < game.world.width; i += 64){
+        for (var j=0; j < game.world.height; j += 64){
+            if (Math.random() > 0.95){
+                var rock = rocks.create(i, j, 'rock1');
+                rock.body.immovable = true;
+                
+            }
+        }
+    }
+}
+                
+        
+function onBulletHitsRock(bullet, rock){
+    bullet.kill();
+    rock.destroy();
 }
